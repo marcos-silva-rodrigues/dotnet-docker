d7e4b80cde9334822147af84451d7781d3c97bf0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-07-28 22:33:42 -0300

    add db

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using myWebApp.Models;
+using myWebApp.Data;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+// Add services to the container.
+builder.Services.AddDbContext<SchoolContext>(options =>
+   options.UseNpgsql(builder.Configuration.GetConnectionString("SchoolContext")));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // add 10 seconds delay to ensure the db server is up to accept connections
+        // this won't be needed in real world application
+        System.Threading.Thread.Sleep(10000);
+        var context = services.GetRequiredService<SchoolContext>();
+        var created = context.Database.EnsureCreated();
+
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
